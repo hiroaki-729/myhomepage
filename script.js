606cc6a71b90ec332c3c8b96288c38b8a78cc7ce
hiropi20020729@icloud.com <hiroaki-729>
2025-08-07 13:39:53 +0900

    更新

--- script.js
+++ script.js
@@ -1,5 +1,23 @@
-document.getElementById('changeColorBtn').addEventListener('click',function(){
-    var randomColor = '#' + Math.floor
-    (Math.random()*16777215).toString(16);
-    document.body.style.backgroundColor = randomColor;
-})
+const lists=[
+    {
+        url:"img/top1.png",
+        position:"center 63%"
+    },
+    {
+        url:"img/top2.png",
+        position:"center 63%"
+    }
+]
+let index=0;
+const interval = 5000; // 5秒ごとに切り替え
+const header = document.getElementById('headerbackground'); // id取得
+
+function changeBackground() {
+    const current = lists[index];
+    header.style.backgroundImage = `url(${current.url})`;
+    header.style.backgroundPosition = current.position;
+    // background-sizeはstyle.cssで定義済みなのでここでは省略可
+    index = (index + 1) % lists.length;
+}
+changeBackground();
+setInterval(changeBackground, interval); //一定時間ごとに関数実行
